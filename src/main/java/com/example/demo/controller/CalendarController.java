85ee0d3a9065c34af757e630fb529688b8b32552
Chris Kolawole <ckolawole@gmail.com>
2025-02-08 08:20:56 -0500

    Updated Controller to add get all events

--- src/main/java/com/example/demo/controller/CalendarController.java
+++ src/main/java/com/example/demo/controller/CalendarController.java
@@ -2,20 +2,27 @@ package com.example.demo.controller;
 
 
 import com.example.demo.service.GoogleCalendarService;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.Events;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping("/calendar")
 public class CalendarController {
 
     @Autowired
     private GoogleCalendarService calendarService;
+
+
+
 
     // Use @ResponseBody to return plain text or JSON directly.
     @RequestMapping(value = "/book", method = RequestMethod.POST)
@@ -27,5 +34,11 @@ public class CalendarController {
 
         String result = calendarService.createEvent(summary, startTime, endTime);
         return result;
+    }
+
+    @GetMapping("/events")
+    public ResponseEntity<List<Event>> getAllEvents() {
+        List<Event> events = calendarService.getAllEvents();
+        return ResponseEntity.ok(events);
     }
 }
